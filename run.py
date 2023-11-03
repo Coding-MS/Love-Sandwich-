41a917d37df2f9aa85d0db2ae9c2653861e341a0
Matthew  Smith <Matthew.smith102023@outlook.com>
2023-11-03 12:12:58 +0000

    Validated Input to say when data is valid using a while loop

--- run.py
+++ run.py
@@ -14,14 +14,19 @@ SHEET = GSPREAD_Cilent.open('love_sandwiches')
 
 def get_sales_data():
     """ Get sales figures input from the user """
-    print ("Please enter sales data from the last market")
-    print ("Data should be six numbers,separated by commas")
-    print ("Example: 35,40,30,66,70,32\n")
+    while True: 
+        print ("Please enter sales data from the last market")
+        print ("Data should be six numbers,separated by commas")
+        print ("Example: 35,40,30,66,70,32\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
 
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break 
+    return sales_data
 
 def validate_data(values):
    """ Validation function """
@@ -31,16 +36,13 @@ def validate_data(values):
             raise ValueError(
                 f"Exactly 6 values required, you have provided {len(values)}"
              )
-        for item in values: 
-            if not isinstance(item,(int,float)):
-                raise TypeError(
-                    f"All values must be numbers"
-                )
-
    except ValueError as e:
        print (f"Invalid data: {e}, please try again \n")
+       return False
+
+   return True 
 
 
-get_sales_data()
+data = get_sales_data()
 
 
